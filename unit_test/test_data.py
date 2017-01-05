61b599e7ee4b7fa14fa5acacf14a90781b8bbda4
lucacimo <luca.cimonetti@gmail.com>
2017-01-05 11:35:38 +0100

    test script
    Completed test script

--- unit_test/test_data.py
+++ unit_test/test_data.py
@@ -28,31 +28,69 @@ users = \
         },
     ]
 
+dates = ["2017-02-01", "2017-02-02", "2017-02-03"]
+
 workouts = \
-           [{
-        "type":"cycling",
-        "description":"Normal endurance ride",
-        "distance":60,
-        "duration":"2:00:15",
-        "averagepower":200,
-        "averageheartrate":140,
-        "calories":500,
+            [{
+                "type":"cycling",
+                "description":"Normal endurance ride",
+                "distance":60,
+                "duration":"2:00:15",
+                "averagepower":200,
+                "averageheartrate":140,
+                "calories":1500,
 
-        "powerzones":{
-            "zone_5":"0:00:00",
-            "zone_4":"0:10:15",
-            "zone_1":"0:10:00",
-            "zone_3":"0:10:00",
-            "zone_2":"0:10:00"
-        },
+                "powerzones":{
+                    "zone_5":"0:00:00",
+                    "zone_4":"0:10:15",
+                    "zone_1":"0:10:00",
+                    "zone_3":"0:10:00",
+                    "zone_2":"0:10:00"
+                },
 
-        "heartratezones":{
-            "zone_5a":"0:00:00",
-            "zones_5c":"0:00:00",
-            "zones_5b":"0:00:00",
-            "zone_4":"0:15:15",
-            "zone_1":"0:10:00",
-            "zone_3":"0:10:00",
-            "zone_2":"0:10:00"
-        }
-}]
+                "heartratezones":{
+                    "zone_5a":"0:00:00",
+                    "zones_5c":"0:00:00",
+                    "zones_5b":"0:00:00",
+                    "zone_4":"0:15:15",
+                    "zone_1":"0:10:00",
+                    "zone_3":"0:10:00",
+                    "zone_2":"0:10:00"
+                }
+            },
+            {
+                "type": "running",
+                "description": "Interval training",
+                "distance": 10,
+                "duration": "0:45:30",
+                "averageheartrate": 150,
+                "calories": 500,
+
+                "heartratezones": {
+                    "zone_5a": "0:00:00",
+                    "zones_5c": "0:00:00",
+                    "zones_5b": "0:00:00",
+                    "zone_4": "0:15:15",
+                    "zone_1": "0:10:00",
+                    "zone_3": "0:10:00",
+                    "zone_2": "0:10:00"
+                }
+            },
+{
+                "type": "swimming",
+                "description": "Technique session",
+                "distance": 2,
+                "duration": "0:40:15",
+                "averageheartrate": 145,
+                "calories": 450,
+
+                "heartratezones": {
+                    "zone_5a": "0:00:00",
+                    "zones_5c": "0:00:00",
+                    "zones_5b": "0:00:00",
+                    "zone_4": "0:15:15",
+                    "zone_1": "0:10:00",
+                    "zone_3": "0:10:00",
+                    "zone_2": "0:10:00"
+                }
+            }]
--- unit_test/test_script.py
+++ unit_test/test_script.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from test_data import users
+from test_data import users, workouts, dates
 
 # create the session
 s = requests.Session()
@@ -60,9 +60,76 @@ def delete_users():
         print_response(r)
 
 
+def post_workouts():
+    i = 0
+    for workout in workouts:
+        parameters={'user':users[0]['name'], 'status':"completed", 'date': dates[i]}
+        r = s.post('http://127.0.0.1:8080/workouts', params=parameters, json=workouts[i])
+        print_request(r, "POST", workouts[i])
+        print_response(r)
+        i +=1
+
+
+def get_workouts():
+    i = 0
+    for workout in workouts:
+        parameters = {'user': users[0]['name'], 'status': "completed", 'date': dates[i]}
+        r = s.get('http://127.0.0.1:8080/workouts', params=parameters, json=workouts[i])
+        print_request(r, "GET")
+        print_response(r)
+        i += 1
+
+
+def put_workouts():
+    workouts[0]['distance'] = 80
+    workouts[1]['distance'] = 12
+    workouts[2]['distance'] = 1.5
+    i = 0
+    for workout in workouts:
+        parameters = {'user': users[0]['name'], 'status': "completed", 'date': dates[i]}
+        r = s.put('http://127.0.0.1:8080/workouts', params=parameters, json=workouts[i])
+        print_request(r, "PUT", workouts[i])
+        print_response(r)
+        i += 1
+
+
+def delete_workouts():
+    i = 0
+    for workout in workouts:
+        parameters = {'user': users[0]['name'], 'status': "completed", 'date': dates[i]}
+        r = s.delete('http://127.0.0.1:8080/workouts', params=parameters, json=workouts[i])
+        print_request(r, "DELETE", workouts[i])
+        print_response(r)
+        i += 1
+
+
+def compute_over_all_statistics():
+    parameters = {'user': users[0]['name']}
+    r = s.get('http://127.0.0.1:8080/statistics', params=parameters)
+    print_request(r, "GET")
+    print_response(r)
+
+
+def compute_statistics_by_sport():
+    parameters = {'user': users[0]['name']}
+    for workout in workouts:
+        parameters = {'user': users[0]['name'], 'sport': workout['type']}
+        r = s.get('http://127.0.0.1:8080/statistics', params=parameters)
+        print_request(r, "GET")
+        print_response(r)
+
+
 if __name__ == '__main__':
     post_users()
     get_users()
     put_users()
     get_users()
-    delete_users()
+    #delete_users()
+    post_workouts()
+    get_workouts()
+    put_workouts()
+    get_workouts()
+    delete_workouts()
+    post_workouts()
+    compute_over_all_statistics()
+    compute_statistics_by_sport()
